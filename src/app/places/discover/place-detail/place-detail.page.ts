662ab8633343eb032b121f15f658a537b370563e
ashiqxq <ashiqxq@gmail.com>
2021-07-21 14:58:51 +0530

    actionsheetcontroller-147

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
+import { ActionSheetController, ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 import { Place } from '../../places.model';
 import { PlacesService } from '../../places.service';
@@ -15,7 +15,8 @@ export class PlaceDetailPage implements OnInit {
   constructor(private router: Router, private navCtrl: NavController,
               private route: ActivatedRoute,
               private placesService: PlacesService,
-              private modalCtrl: ModalController) { }
+              private modalCtrl: ModalController,
+              private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap =>{
@@ -32,6 +33,32 @@ export class PlaceDetailPage implements OnInit {
     // we get the going back navigation as well
     // this.navCtrl.navigateBack('/places/tabs/discover');
     // this.navCtrl.pop();
+    this.actionSheetCtrl.create({
+      header: 'Choose an Action',
+      buttons: [
+        {
+          text: 'Select Date',
+          handler: ()=>{
+            this.openBookingModal('select');
+          }
+        },
+        {
+          text: 'Random Date',
+          handler: ()=>{
+            this.openBookingModal('random');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'destructive'
+        }
+      ]
+    }).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+  }
+  openBookingModal(mode: 'select'|'random'){
+    console.log(mode);
     this.modalCtrl.create({
       component: CreateBookingComponent,
       componentProps: {selectedPlace: this.place},
